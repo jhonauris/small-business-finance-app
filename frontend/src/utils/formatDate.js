5043d3b7cfcfab3e8b117dc8232fac917bc1ce3c
jhonauris <jhonauris.segura16@gmail.com>
2024-11-01 09:51:59 -0400

    formatDate now returns the next date.

--- frontend/src/utils/formatDate.js
+++ frontend/src/utils/formatDate.js
@@ -1,5 +1,6 @@
 export function formatDate(timestamp) {
   const date = new Date(parseInt(timestamp)); // Parse the timestamp to ensure it's an integer representing milliseconds
+  date.setDate(date.getDate() + 1); // Move the date to the next day
   const options = { day: "2-digit", month: "short", year: "numeric" };
   return date.toLocaleDateString("en-US", options);
 }
@@ -7,4 +8,4 @@ export function formatDate(timestamp) {
 // Example usage:
 const timestamp = 1704067200000;
 const formattedDate = formatDate(timestamp);
-console.log(formattedDate); // Output: "12 Dec 2023"
+console.log(formattedDate); //Jan 01, 2024
